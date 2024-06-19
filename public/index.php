ac2c7713fb5dc76b9696cc9fb65dbca19c05775a
dzmitryulasenka-innowise <dzmitry.ulasenka@innowise.com>
2024-06-19 16:26:22 +0300

    initial setup

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title> Имя страницы </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+
+</html>
+
+
+
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+
+$app = new \App\App();
+$app->run();
