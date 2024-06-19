2d292548c04aee0bddb158d97c8f14346cfd2221
dzmitryulasenka-innowise <dzmitry.ulasenka@innowise.com>
2024-06-19 12:21:53 +0300

    add 2 decision

--- App/Models/Task5.php
+++ App/Models/Task5.php
@@ -9,7 +9,7 @@ class Task5
     public function main(int $left, int $right): array
     {
         if ($left > $right) {
-            list($left,$right)=[$right,$left];
+            list($left, $right) = [$right, $left];
             $result = $this->createFilledArray($left, $right);
             return array_reverse($result);
         }
@@ -19,19 +19,55 @@ class Task5
 
     private function createFilledArray(int $left, int $right): array
     {
+
         if ($left === $right) {
             return [$left];
         }
         if (abs(($left - $right)) < 2) {
             return [$left, $right];
         }
+
         $midLeft = (int)(floor($left + $right) / 2);
         $midRight = (int)(ceil($left + $right) / 2);
 
         if ($midLeft === $midRight) {
-            return array_merge_recursive($this->main($left, $midRight - 1), [$midRight], $this->main($midRight + 1, $right));
+            return array_merge_recursive($this->createFilledArray($left, $midRight - 1),
+                [$midRight],
+                $this->createFilledArray($midRight + 1, $right));
+        }
+
+        return array_merge_recursive($this->createFilledArray($left, $midLeft), $this->createFilledArray($midRight, $right));
+    }
+
+    public function main2(int $left, int $right): array
+    {
+
+        if ($left === $right) {
+            return [$left];
+        }
+        if (abs(($left - $right)) < 2) {
+            return [$left, $right];
+        }
+
+        $minus = -1;
+        $plus = 1;
+        $midLeft = (int)(floor($left + $right) / 2);
+        $midRight = (int)(ceil($left + $right) / 2);
+
+        if ($left > $right) {
+            $minus = 1;
+            $plus = -1;
+            $midLeft = (int)(ceil($left + $right) / 2);
+            $midRight = (int)(floor($left + $right) / 2);
+        }
+
+        if ($midLeft === $midRight) {
+            return array_merge_recursive($this->main($left, $midRight + $minus),
+                [$midRight],
+                $this->main($midRight + $plus, $right));
         }
 
         return array_merge_recursive($this->main($left, $midLeft), $this->main($midRight, $right));
     }
+
 }
--- indexTask5.php
+++ indexTask5.php
@@ -7,5 +7,7 @@ use App\Models\Task5;
 
 $task5 = new Task5();
 var_dump($task5->main( 6,3));
+var_dump($task5->main( -3,3));
 
-//var_dump($task5->main2( 6,-3));
+//var_dump($task5->main2( 6,3));
+//var_dump($task5->main2( -3,3));
