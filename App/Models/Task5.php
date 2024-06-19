96cbc5ba1161a63cd259f2632f7d9d5f5a262e48
dzmitryulasenka-innowise <dzmitry.ulasenka@innowise.com>
2024-06-19 11:55:49 +0300

    add full decision

--- App/Models/Task5.php
+++ App/Models/Task5.php
@@ -8,14 +8,23 @@ class Task5
 {
     public function main(int $left, int $right): array
     {
+        if ($left > $right) {
+            list($left,$right)=[$right,$left];
+            $result = $this->createFilledArray($left, $right);
+            return array_reverse($result);
+        }
 
+        return $this->createFilledArray($left, $right);
+    }
+
+    private function createFilledArray(int $left, int $right): array
+    {
         if ($left === $right) {
             return [$left];
         }
         if (abs(($left - $right)) < 2) {
             return [$left, $right];
         }
-
         $midLeft = (int)(floor($left + $right) / 2);
         $midRight = (int)(ceil($left + $right) / 2);
 
@@ -24,30 +33,5 @@ class Task5
         }
 
         return array_merge_recursive($this->main($left, $midLeft), $this->main($midRight, $right));
-    }
-
-    public function main2(int $left, int $right): array
-    {
-        $result = [];
-        if ($left === $right) {
-            $result = [$left];
-        } elseif (abs(($left - $right)) < 2) {
-            $result = [$left, $right];
-        } else {
-            $midLeft = (int)(floor($left + $right) / 2);
-            $midRight = (int)(ceil($left + $right) / 2);
-            if ($midLeft === $midRight) {
-                $result = array_merge_recursive($this->main($left, $midRight - 1), [$midRight], $this->main($midRight + 1, $right));
-            } else {
-                $result = array_merge_recursive($this->main($left, $midLeft), $this->main($midRight, $right));
-            }
-        }
-        print_r($result);
-
-//        if ($left > $right) {
-//            $result = array_reverse($result);
-//        }
-
-        return $result;
     }
 }
--- indexTask5.php
+++ indexTask5.php
@@ -6,18 +6,6 @@ use App\Models\Task5;
 
 
 $task5 = new Task5();
-//var_dump($task5->main( -3,6));
+var_dump($task5->main( 6,3));
 
-var_dump($task5->main2( 6,-3));
-
-
-
-//$frenchCities = ['paris', 'marseille'];
-//
-//$cities = array_merge(['milan', 'rome'], $frenchCities);
-//
-//var_dump($cities);
-//
-//$array = [[1],[2],[3], [4,5,6]];
-//$newArray = array_merge_recursive([1], [2], [3], [4,5,6]);
-//var_dump($newArray);
+//var_dump($task5->main2( 6,-3));
